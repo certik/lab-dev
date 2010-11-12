12d3ab7842fe9f467dccbd962f72eccd55f90598
ondrej <ondrej@lab.rd.unr.edu>
2010-11-12 09:27:05 -0800

    Start and stop the service

--- update.sh
+++ update.sh
@@ -13,15 +13,26 @@ if [ "$user" != "$user_allowed" ]; then
     echo "id != $user_allowed, exiting..."
     exit 1
 fi
-echo "Updating repository..."
-cd /home/ondrej/repos/femhub-online-lab
-git pull origin master
-echo "  Done."
 
-echo "  Updating the online lab:"
-git clean -dfx
+
 CONFIG_DIR=c
+cd /home/ondrej/repos/femhub-online-lab
+echo "Killing the old lab:"
+bin/onlinelab service stop --home=$CONFIG_DIR/service-home
+bin/onlinelab core stop --home=$CONFIG_DIR/core-home
+echo "Print all running online lab processes (should only show 'grep onlinelab')"
+ps aux | grep onlinelab
+
+echo "Updating repository..."
+git clean -dfx
+git pull origin master
+
+echo "Updating the online lab:"
 mkdir $CONFIG_DIR
 cp -r ui $CONFIG_DIR/
 bin/onlinelab core init --home=$CONFIG_DIR/core-home --ui-path=$CONFIG_DIR/ui
 bin/onlinelab service init --home=$CONFIG_DIR/service-home
+
+echo "Starting the online lab:"
+bin/onlinelab core start --home=$CONFIG_DIR/core-home
+bin/onlinelab service start --home=$CONFIG_DIR/service-home

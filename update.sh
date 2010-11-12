cdadfbeb6fcc0e670fa1678c46932c94f012db06
ondrej <ondrej@lab.rd.unr.edu>
2010-11-12 09:07:48 -0800

    Update

--- update.sh
+++ update.sh
@@ -2,8 +2,17 @@
 
 set -e
 
-echo "Running as:"
-id
+# Change this to the current user, that owns the repos:
+user_allowed="ondrej"
+
+
+user=`id -un`
+echo "Running as: $user"
+# Make sure that we run as $user_allowed
+if [ "$user" != "$user_allowed" ]; then
+    echo "id != $user_allowed, exiting..."
+    exit 1
+fi
 echo "Updating repository..."
 cd /home/ondrej/repos/femhub-online-lab
 git pull origin master
